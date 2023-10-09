66f693f87099ab97d3342d3d9bbfe5defc99477e
onurusluca <uslucaonur@gmail.com>
2023-10-09 18:19:09 +0900

    inital setups 10

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -35,13 +35,13 @@
 
 	<p class="currently-working-on">
 		{$t('home.currentlyWorkingOn')}
-		<a href="/atsumari">Atsumari</a> -
+		<a target="_blank" href="/atsumari">Atsumari</a> -
 		<i>{$t('home.atsumariDescription')}</i>
 	</p>
 
 	<div class="atsumari">
-		<img class="atsumari-demo-img" src={atsumari} alt="Atsumari demo" />
-		<img class="atsumari-demo-img" src={atsumari2} alt="Atsumari demo" />
+		<img src={atsumari} alt="Atsumari demo" />
+		<img src={atsumari2} alt="Atsumari demo" />
 	</div>
 
 	<hr />
@@ -54,14 +54,17 @@
 		<p>
 			{$t('home.techStackDescription2')}
 			<a
+				target="_blank"
 				href="https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/Tutorial/Getting_started_with_WebGL"
 			>
 				WebGL</a
 			>,
-			<a href="https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API">WebRTC</a>,
+			<a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API"
+				>WebRTC</a
+			>,
 			{$t('misc.and')}
 
-			<a href="https://bun.sh/L">Bun</a>.
+			<a target="_blank" href="https://bun.sh/L">Bun</a>.
 		</p>
 	</div>
 
@@ -69,18 +72,31 @@
 
 	<h2>{$t('home.myProjects')}</h2>
 	<div class="hobby-projects">
-		<a href="https://github.com/onurusluca/denmou-company">
-			<img class="atsumari-demo-img" src={denmou} alt="Denmou" />
+		<a target="_blank" class="no-image-project" href="https://github.com/onurusluca/atsumari">
+			First implementation of Atsumari. <br /> Old codebase.
 		</a>
-		<a href="https://github.com/onurusluca/egaki">
-			<img class="atsumari-demo-img" src={egaki} alt="Egaki" />
+
+		<a
+			target="_blank"
+			class="no-image-project"
+			href="https://github.com/onurusluca/nuxt3-boilerplate-karpuz"
+		>
+			Karpuz 🍉 - Opinionated Nuxt 3 Starter
 		</a>
-		<a href="https://github.com/onurusluca/ongaku-music-player"
-			><img class="atsumari-demo-img" src={ongaku} alt="Ongaku" />
+		<a target="_blank" class="no-image-project" href="https://portfolio-v2-onurusluca.vercel.app/">
+			Portfolio V2
 		</a>
-		<a href="https://kaidanfit.netlify.app/"
-			><img class="atsumari-demo-img" src={kaidan} alt="Kaidan" />
+
+		<a target="_blank" href="https://github.com/onurusluca/denmou-company">
+			<img src={denmou} alt="Denmou" />
 		</a>
+		<a target="_blank" href="https://github.com/onurusluca/egaki">
+			<img src={egaki} alt="Egaki" />
+		</a>
+		<a target="_blank" href="https://github.com/onurusluca/ongaku-music-player"
+			><img src={ongaku} alt="Ongaku" />
+		</a>
+		<a target="_blank" href="https://kaidanfit.netlify.app/"><img src={kaidan} alt="Kaidan" /> </a>
 	</div>
 
 	<hr />
@@ -89,16 +105,19 @@
 
 	<div class="hobby-projects">
 		<a
+			target="_blank"
 			href="https://www.figma.com/file/EZLvdvFgHZ7O7mXPaeUJfD/Ennka---Japanese-Enka-Music--Platform-Concept--(%E6%BC%94%E6%AD%8C%E9%9F%B3%E6%A5%BD%E3%83%97%E3%83%A9%E3%83%83%E3%83%88%E3%83%95%E3%82%A9%E3%83%BC%E3%83%A0)-Spotify-Clone?type=design&mode=design"
-			><img class="atsumari-demo-img" src={ennka} alt="Ennka" />
+			><img src={ennka} alt="Ennka" />
 		</a>
 		<a
+			target="_blank"
 			href="https://www.figma.com/file/Ltj16yqUVXDgjXWlzeAhWC/OhBento!---Japanese-Bentou-Shopping-Concept-App-(%E3%81%8A%E5%BC%81%E5%BD%93%E3%82%B7%E3%83%A7%E3%83%83%E3%83%94%E3%83%B3%E3%82%B0%E3%82%B3%E3%83%B3%E3%82%BB%E3%83%97%E3%83%88%E3%82%A2%E3%83%97%E3%83%AA)?type=design&mode=design"
-			><img class="atsumari-demo-img" src={ohbento} alt="Ohbento" />
+			><img src={ohbento} alt="Ohbento" />
 		</a>
 		<a
+			target="_blank"
 			href="https://www.figma.com/file/4Oihnv3HYx5UEEOGAWGIbr/Sosyal---Social-Media-Clones-(%E3%82%BD%E3%83%BC%E3%82%B7%E3%83%A3%E3%83%AB%E3%83%A1%E3%83%87%E3%82%A3%E3%82%A2%E3%81%AE%E3%82%AF%E3%83%AD%E3%83%BC%E3%83%B3)?type=design&mode=design"
-			><img class="atsumari-demo-img" src={sosyal} alt="Sosyal" />
+			><img src={sosyal} alt="Sosyal" />
 		</a>
 	</div>
 </section>
@@ -184,9 +203,23 @@
 			display: flex;
 			align-items: center;
 			justify-content: space-between;
-			gap: 0.5rem;
+			gap: 1rem;
 			flex-wrap: wrap;
 			width: 100%;
+
+			.no-image-project {
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				text-align: center;
+
+				width: 21rem;
+				height: 12.5rem;
+				border-radius: 0.4rem;
+				border: 1px solid var(--accent);
+
+				cursor: pointer;
+			}
 			a {
 				img {
 					width: 21rem;
