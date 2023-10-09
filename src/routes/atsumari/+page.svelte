32321ae22e6a109fe7c2d0bedb89d6f328310227
onurusluca <uslucaonur@gmail.com>
2023-10-09 17:32:05 +0900

    inital setups 9

--- src/routes/atsumari/+page.svelte
+++ src/routes/atsumari/+page.svelte
@@ -3,4 +3,19 @@
 	<meta name="description" content="Atsumari" />
 </svelte:head>
 
-<div class="text-column">Atsumari</div>
+<div class="atsumari">
+	<video
+		src="https://private-user-images.githubusercontent.com/51825926/273531643-1fb719d4-7870-4005-8a08-42b49e9ff2fe.mp4?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTEiLCJleHAiOjE2OTY4NDA0NzEsIm5iZiI6MTY5Njg0MDE3MSwicGF0aCI6Ii81MTgyNTkyNi8yNzM1MzE2NDMtMWZiNzE5ZDQtNzg3MC00MDA1LThhMDgtNDJiNDllOWZmMmZlLm1wND9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFJV05KWUFYNENTVkVINTNBJTJGMjAyMzEwMDklMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjMxMDA5VDA4MjkzMVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWE2MDEyNDc1ZDA5MzM1NmNlMmY2NWEyMmZkYTBhYWM5ZjM5ZGNmMDE0ZWZlYzMxMTFlM2UyMjRhMmViMGFmY2EmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.xuualnYL4-wnT_pHJysJ196CDn9tcHT287SLcucPY0s"
+		autoplay
+		loop
+		muted
+		controls
+		style="width: 100%; height: auto;"
+	/>
+</div>
+
+<style lang="scss">
+	.atsumari {
+		margin-top: 3rem;
+	}
+</style>
